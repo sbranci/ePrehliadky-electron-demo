d63da7d60a22274ca1861d1d133463ca95dee729
Branislav Sipula <branislav.sipula@plaut.sk>
2023-01-18 18:03:14 +0100

    uprava kodu podla poslednej casti

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { autoUpdater } = require('electron-updater')
-
 const log = require('electron-log')
+
 log.transports.file.resolvePath = () => path.join('C:/development/ePrehliadky/ePrehliadky-electron-demo/', '/logs/main.log');
-log.info('Hello, log')
+log.log("Application version = " + app.getVersion())
 log.warn('Some problem appears')
 
 let win;
 
 function createWindow() {
   win = new BrowserWindow({width:300, height:400})
-  console.log(path.join(__dirname, 'index.html'))
+
   win.loadFile(path.join(__dirname, 'index.html'))
 }
+
+autoUpdater.on("checking-for-update", () => {
+  log.info("Checking for update...");
+})
+
+autoUpdater.on("update-available", (info) => {
+  log.info("Update available.");
+})
+
+autoUpdater.on("update-not-available", (info) => {
+  log.info("Update not available.");
+})
+
+autoUpdater.on("error", (err) => {
+  log.info("Error in auto-update. " + err);
+})
+
+autoUpdater.on("download-progress", (progressTrack) => {
+  log.info("Download progress");
+  log.info(progressTrack);
+})
+
+autoUpdater.on("update-downloaded", (info) => {
+  log.info("Update downloaded.");
+})
 
 app.on('ready', () => {
   createWindow()
   autoUpdater.checkForUpdatesAndNotify()
 })
-
-autoUpdater.on("update-available"), () => {
-  log.info("update-available")
-}
-
-autoUpdater.on("checking-for-update"), () => {
-  log.info("checking-for-update")
-}
-
-autoUpdater.on("download-progress"), () => {
-  log.info("download-progress")
-}
-
-autoUpdater.on("update-downloaded"), () => {
-  log.info("update-downloaded")
-}
