0f560a68946e655299d9e977a9780f16770cda0d
Branislav Sipula <branislav.sipula@plaut.sk>
2023-01-24 11:19:10 +0100

    8.0.14
    Notification

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, dialog, BrowserWindow, ipcMain } = require('electron')
+const { app, dialog, BrowserWindow, ipcMain, Notification  } = require('electron')
 const path = require('path')
 const { autoUpdater } = require('electron-updater')
 const log = require('electron-log')
@@ -6,8 +6,8 @@ const log = require('electron-log')
 log.transports.file.resolvePath = () => path.join('C:/development/ePrehliadky/ePrehliadky-electron-demo/', '/logs/main.log');
 
 let mainWindow;
-// const NOTIFICATION_TITLE = 'Update Notification'
-// const NOTIFICATION_BODY = 'Notification from the Main process, a new version is being downloaded.'
+const NOTIFICATION_TITLE = 'Update Notification'
+const NOTIFICATION_BODY = 'A new version is being downloaded.'
 
 function createWindow() {
   // Create the browser window.
@@ -33,14 +33,13 @@ function createWindow() {
 
   log.log("-----------------------------------------");
   log.log("Application version = " + app.getVersion());
-  log.log(__dirname);
   
   autoUpdater.checkForUpdates()
 }
 
-// function showNotification() {
-//   new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
-// }
+function showNotification() {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
 
 app.on("ready", () => {
   createWindow();
@@ -79,7 +78,7 @@ autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
 		detail: 'A new version is being downloaded.'
 	}
   log.info("Update available.");
-  // showNotification();
+  showNotification();
 	dialog.showMessageBox(dialogOpts, (response) => {
 
   });
